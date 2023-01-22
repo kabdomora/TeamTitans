e5ca0e9b51b05ec74ae0ddb43cacb1d017d5eae2
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-01-21 17:21:54 -0800

    packages added to index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+const inquirer = require('inquirer');
+const generateFile = require('./utils/generateFile');
+const fs = require('fs');
+const process = require('process');
+const path = require('path');

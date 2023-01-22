2b71583ec6675e90cd1ad76a1b4d0c98e654b670
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-01-21 21:52:36 -0800

    linked to tailwind

--- utils/generateFile.js
+++ utils/generateFile.js
@@ -1,7 +1,28 @@
 function generateFile(data) {
   // function here
   const employees = JSON.parse(data);
-  return `${employees[0].manager_name}`;
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Contact Us 📧</title>
+      <link rel="stylesheet" href="./assets/css/style.css" />
+      <script src="https://cdn.tailwindcss.com?plugins=typography,line-clamp"></script>
+  </head>
+  <body>
+      <section>
+        // Team manager info here: ${employees[0].manager_name}
+      </section>
+      <section>
+        // Employees here
+      </section>
+  </body>
+  </html>
+  
+  
+  `;
 
 }
 

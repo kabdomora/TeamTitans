55eef51c3ca9dd29f2b867c5627d026af5d9c635
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-02-06 22:35:47 -0800

    intern and engineer cards produced dynamically

--- utils/generateFile.js
+++ utils/generateFile.js
@@ -2,52 +2,78 @@ function generateFile(data) {
   // function here
   const employees = JSON.parse(data);
 
-  employees.forEach((value, index) => {
-    if (index > 0) {
-      let title = value.title;
-      let ename = value.engineer_name;
-      let iname = value.intern_name;
-      let eid = value.engineer_id;
-      let iid = value.intern_id;
-      let eemail = value.engineer_email;
-      let iemail = value.intern_email;
-      let github = value.github;
-      let school = value.school;
+  const engineers = [];
+  const interns = [];
 
-
-      if (title === "Engineer") {
-        return `
-        <section class="card flex flex-col border-dashed border-2 border-stone-400 p-2.5 mx-5 w-28 basis-80">
-          <header class="card-header relative border-solid border-stone-400 border rounded-md p-1 font-extrabold text-teal-500">${ename}</header>
-          <h3>Engineer</h3>
+  function renderEmployees(employees) {
+    for(var i=0; i < employees.length; i++) {
+      if (employees[i].title === "Engineer") {
+        const eperson =  `
+        <section class="card flex flex-col border-dashed border-2 border-stone-400 p-2.5 mx-5 w-28 basis-80 p-8">
+          <header class="card-header relative border-solid border-stone-400 border rounded-md p-1 font-extrabold text-teal-500">${employees[i].engineer_name}</header>
+          <h3>${employees[i].title}</h3>
           <div class="pre bg-gradient-to-t from-cyan-500 to-blue-500 rounded-md text-stone-400">
             <ul>
-              <li class="text-stone-400">ID# ${eid}</li>
-              <li class="text-stone-400"><a href="mailto:${eemail}">Email: ${eemail}</a></li>
-              <li class="text-stone-400"><a href="https://github.com/${github}">${github}</a></li>
+              <li class="text-stone-400">ID# ${employees[i].engineer_id}</li>
+              <li class="text-stone-400"><a href="mailto:${employees[i].engineer_email}">Email: ${employees[i].engineer_email}</a></li>
+              <li class="text-stone-400"><a href="https://github.com/${employees[i].github}">${employees[i].github}</a></li>
+            </ul>
+          </div>
+        </section>
+        `   
+
+        engineers.push(eperson);
+      } else if (employees[i].title === "Intern") {
+        const iperson = `
+        <section class="card flex flex-col border-dashed border-2 border-stone-400 p-2.5 mx-5 w-28 basis-80 p-8">
+          <header class="card-header relative border-solid border-stone-400 border rounded-md p-1 font-extrabold text-teal-500">${employees[i].intern_name}</header>
+          <h3>${employees[i].title}</h3>
+          <div class="pre bg-gradient-to-t from-cyan-500 to-blue-500 rounded-md text-stone-400">
+            <ul>
+              <li class="text-stone-400">ID# ${employees[i].intern_id}</li>
+              <li class="text-stone-400"><a href="mailto:${employees[i].intern_email}">Email: ${employees[i].intern_email}</a></li>
+              <li class="text-stone-400">Current School: ${employees[i].school}</li>
             </ul>
           </div>
         </section>
         `
-        
-      } else if (title === "Intern") {
-        return `
-        <section class="card flex flex-col border-dashed border-2 border-stone-400 p-2.5 mx-5 w-28 basis-80">
-          <header class="card-header relative border-solid border-stone-400 border rounded-md p-1 font-extrabold text-teal-500">${iname}</header>
-          <h3>Engineer</h3>
-          <div class="pre bg-gradient-to-t from-cyan-500 to-blue-500 rounded-md text-stone-400">
-            <ul>
-              <li class="text-stone-400">ID# ${iid}</li>
-              <li class="text-stone-400"><a href="mailto:${iemail}">Email: ${iemail}</a></li>
-              <li class="text-stone-400">Current School: ${school}</li>
-            </ul>
-          </div>
-        </section>
-        `
+
+        interns.push(iperson);
       }
     }
-    
-  });
+
+    const allEngineers = engineers.join('');
+    const allInterns = interns.join('');
+
+    return `
+    <div class="p-8 grid grid-cols-4">
+      <div class="col-span-1">
+        <h1 class="text-2xl my-2.5 text-left text-cyan-400">Engineers</h2>
+      </div>
+
+      <div class="col-span-3">
+        <div id="engineers" class="flex flex-row flex-wrap space-x-2.5 space-y-2.5">              
+        ${allEngineers}      
+        </div>
+      </div>
+    </div>
+
+    <div class="p-8 grid grid-cols-4">
+      <div class="col-span-1">
+        <h1 class="text-2xl my-2.5 text-left text-cyan-400">Interns</h2>
+      </div>
+
+      <div class="col-span-3">
+        <div id="interns" class="flex flex-row flex-wrap space-x-2.5 space-y-2.5">              
+        ${allInterns}      
+        </div>
+      </div>
+    </div>
+    `
+
+
+  }
+
   
   return `<!DOCTYPE html>
   <html lang="en">
@@ -59,7 +85,6 @@ function generateFile(data) {
       <link rel="stylesheet" href="./assets/css/style.css" />
       <script src="https://cdn.tailwindcss.com?plugins=typography,line-clamp"></script>
   </head>
-  ${data}
   <body>
       <header>
         <h1 class="text-2xl my-2.5 text-center text-cyan-400">Meet The Team</h1>
@@ -80,16 +105,8 @@ function generateFile(data) {
           </section>
         </div>
         
-        <div id="engineers" class="flex flex-row flex-wrap space-x-2.5 space-y-2.5">
-          
-            // Engineers here
-          
-        </div>
-        <div id="interns" class="flex flex-row flex-wrap space-x-2.5 space-y-2.5">
-          
-            // Interns here
-          
-        </div>
+        ${renderEmployees(employees)}
+
       </main>
   </body>
   </html>

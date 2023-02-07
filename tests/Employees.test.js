1baabc56a96940b824c33acb57dd8b01d75d71f0
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-02-07 00:50:34 -0800

    tests finalized

--- tests/Employees.test.js
+++ tests/Employees.test.js
@@ -4,6 +4,8 @@ const Intern = require('../lib/Intern');
 const Manager = require('../lib/Manager');
 
 describe("Employee", () => {
+    const employee = new Employee("Kayla", "1234", "kayla@mail.com");
+    
     describe("newEmployee", () => {
         it("should create a new employee object", () => {
             const object = new Employee();
@@ -14,30 +16,35 @@ describe("Employee", () => {
 
     describe("getName()", () => {
         it("should return the employee name", () => {
-            const name = "Sample Name";
+            
+            const name = "Kayla";
 
-            expect(object.getName()).toBe(name);
+            expect(employee.getName()).toBe(name);
         });
     });
 
     describe("getId()", () => {
         it("should return the employee id", () => {
-            const id = "123";
+            
+            const id = "1234";
 
-            expect(object.getId()).toBe(id);
+            expect(employee.getID()).toBe(id);
         });
     });
 
     describe("getEmail()", () => {
         it("should return the employee email", () => {
-            const email = "address@mail.com";
+            
+            const email = "kayla@mail.com";
 
-            expect(object.getEmail()).toBe(email);
+            expect(employee.getEmail()).toBe(email);
         });
     });
 });
 
 describe("Engineer", () => {
+    const engineer = new Engineer("Kayla", "1234", "kayla@mail.com", "kabdomora");
+
     describe("newEngineer", () => {
         it("should create a new engineer object", () => {
             const object = new Engineer();
@@ -48,21 +55,23 @@ describe("Engineer", () => {
 
     describe("getGithub()", () => {
         it("should return the github id", () => {
-            const gitId = "GittyId";
+            const gitId = "kabdomora";
 
-            expect(object.getGithub()).toBe(gitId);
+            expect(engineer.getGithub()).toBe(gitId);
         });
     });
 
     describe("getRole()", () => {
         it("should return the employee role as Engineer", () => {
 
-            expect(object.getRole()).toBe("Engineer");
+            expect(engineer.getRole()).toBe("Engineer");
         });
     });
 });
 
 describe("Intern", () => {
+    const intern = new Intern("Kayla", "1234", "kayla@mail.com", "UCLA");
+
     describe("newIntern", () => {
         it("should create a new Intern object", () => {
             const object = new Intern();
@@ -75,19 +84,21 @@ describe("Intern", () => {
         it("should return the school name", () => {
             const school = "UCLA";
 
-            expect(object.getGithub()).toBe(school);
+            expect(intern.getSchool()).toBe(school);
         });
     });
 
     describe("getRole()", () => {
         it("should return the employee role as Intern", () => {
 
-            expect(object.getRole()).toBe("Intern");
+            expect(intern.getRole()).toBe("Intern");
         });
     });
 });
 
 describe("Manager", () => {
+    const manager = new Manager("Kayla", "1234", "kayla@mail.com", "3124");
+
     describe("newManager", () => {
         it("should create a new Manager object", () => {
             const object = new Manager();
@@ -99,7 +110,7 @@ describe("Manager", () => {
     describe("getRole()", () => {
         it("should return the employee role as Manager", () => {
 
-            expect(object.getRole()).toBe("Manager");
+            expect(manager.getRole()).toBe("Manager");
         });
     });
 });
